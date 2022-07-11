0838a665f2c7176c626a3f0bf3ed40e3da135fd8
dliulabs <david.s.liu@gmail.com>
2022-07-11 01:01:22 +0000

    add port scaner

---
+++ 001-port-scan/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+type Result struct {
+	addr string
+	open bool
+	err  error
+}
+
+var results = make(map[string]Result)
+
+func Scan(addr string, m *sync.Mutex) {
+	_, err := net.Dial("tcp", addr)
+	m.Lock()
+	defer m.Unlock()
+	results[addr] = Result{
+		addr: addr,
+		open: err == nil,
+		err:  err,
+	}
+	return
+}
+
+func main() {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	host := "scanme.nmap.org"
+	for port := 20; port < 1000; port++ {
+		wg.Add(1)
+		addr := fmt.Sprintf("%s:%d", host, port)
+		go func(addr string) {
+			Scan(addr, &mu)
+			wg.Done()
+		}(addr)
+	}
+	wg.Wait()
+	for addr, result := range results {
+		if result.open {
+			fmt.Printf("%s: %v\n", addr, result.open)
+		}
+	}
+}
