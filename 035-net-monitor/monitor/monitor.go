b3d1232c2df7e19e303e4c988f553a81d8b0d7ea
dliulabs <david.s.liu@gmail.com>
2022-07-19 02:25:12 +0000

    add net monitor

--- 035-net-monitor/monitor/monitor.go
+++ 035-net-monitor/monitor/monitor.go
@@ -7,6 +7,11 @@ type Monitor struct {
 }
 
 // Write implements the io.Writer interface.
+// make monitor non-intrusive, always return error = nil
 func (m *Monitor) Write(p []byte) (int, error) {
-	return len(p), m.Output(2, string(p))
+	err := m.Output(2, string(p))
+	if err != nil {
+		log.Println(err) // use the log package’s default Logger
+	}
+	return len(p), nil
 }
